23c51cd42cd4e266bc5b878e7a7d5bd72453ddc6
douxxtech <douxx@douxx.tech>
2026-03-18 18:18:16 +0100

    test update

--- test.asm
+++ test.asm
@@ -4,11 +4,14 @@
 
 section .data
     string db "SGVsbG8sIFdvcmxkICE=", 0
-    request db "GET / HTTP/1.0", 0xd, 0xa, \
-               "Authorization: Basic aGVsbG9oZWxsbzpteXBhc3M2NQ==", \
-               0xd, 0xa, 0xd, 0xa
+    request db "GET / HTTP/1.1", 0xd, 0xa, \
+               "Host: localhost", 0xd, 0xa, \
+               "User-Agent: curl/8.15.0", 0xd, 0xa, \
+               "Authorization: Basic YWRtaW46YWRtaW4=", 0xd, 0xa, \
+               0xd, 0xa
 
     request_len equ $ - request
+
 
 section .bss
     output resb 258 ; 128 (user) + 1 (:) + 128 (pwd) + null term (1)
@@ -17,6 +20,7 @@ section .text
     global _start
 
 _start:
-    PARSE_AUTH_HEADER request, request_len, output, r10, 257
-    PRINTN output, r10
+    PARSE_AUTH_HEADER request, request_len, output, 258
+    STRLEN output, rcx  ; null-term check
+    PRINTN output, rcx
     EXIT 0
